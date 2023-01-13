3cc22e3f4e73021e655611b615582cc7cf186437
elena <ep.yudzina@gamil.com>
2023-01-13 18:18:09 +0500

    fix typo

--- fastapi_srv/src/api/v1/films.py
+++ fastapi_srv/src/api/v1/films.py
@@ -8,12 +8,13 @@ from .response_models import Film
 router = APIRouter()
 
 
-@router.get('/{film_id}',
-            response_model=Film,
-            summary="Детализация по фильму",
-            description="Детализированая информация по фильму",
-            response_description="Детализированая информация по фильму"
-            )
+@router.get(
+    '/{film_id}',
+    response_model=Film,
+    summary="Детализация по фильму",
+    description="Детализированная информация по фильму",
+    response_description="Детализированная информация по фильму"
+)
 async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
     film = await film_service.get_by_id(film_id)
     if not film:
