ebafddb821d86a329fb4d2aa181ad4cc19099877
PavelAd <adap2@mail.ru>
2023-01-21 10:47:18 +0400

    изменил для использвания common

--- fastapi_srv/src/api/v1/films.py
+++ fastapi_srv/src/api/v1/films.py
@@ -3,6 +3,7 @@ from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, Query
 from services.film import FilmService, get_film_service
+from services.common import Service, get_service
 
 from api.v1.models.query_models import Sort
 from api.v1.models.response_models import Film, BaseFilm
@@ -21,9 +22,10 @@ async def search_films(
         query: str = Query(default=..., title='Что искать'),
         page: int = Query(default=1, ge=1, alias='page[number]', title='Страница'),
         size: int = Query(default=50, ge=1, alias='page[size]', title='Количество фильмов на странице'),
-        film_service: FilmService = Depends(get_film_service)
+        film_service: Service = Depends(get_service)
 ) -> list[BaseFilm]:
-    films = await film_service.get_films_from_elastic(page=page, size=size, query=query)
+    films = await film_service.get_all_from_elastic(page=page, size=size, query=query,
+                                                    fields=['title', 'description'], key='movies')
     if not films:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
 
@@ -37,8 +39,8 @@ async def search_films(
     description="Детализированная информация по фильму",
     response_description="Детализированная информация по фильму"
 )
-async def film_details(film_id: UUID, film_service: FilmService = Depends(get_film_service)) -> Film:
-    film = await film_service.get_by_id(film_id)
+async def film_details(film_id: UUID, film_service: Service = Depends(get_service)) -> Film:
+    film = await film_service.get_by_id(film_id, key='movies')
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
 
@@ -51,17 +53,19 @@ async def film_details(film_id: UUID, film_service: FilmService = Depends(get_fi
     summary="Фильмы",
     description="Фильмы",
     response_description="Фильмы"
+
 )
-async def get_films(
-        genre: UUID = Query(default=None, title='Жанр'),
+async def get_films_by_genre(
+        genre: UUID = Query(default=None, alias='filter[genre]', title='Жанр'),
         sort: Sort = Query(default=Sort.imdb_rating_desc, title='Сортировка'),
         page: int = Query(default=1, ge=1, alias='page[number]', title='Страница'),
         size: int = Query(default=50, ge=1, alias='page[size]', title='Количество фильмов на странице'),
         film_service: FilmService = Depends(get_film_service)
 ) -> list[BaseFilm]:
 
-    films = await film_service.get_films_from_elastic(page=page, size=size, genre_id=genre, sort_=sort.value)
+    films = await film_service.get_films_genre_sort(page=page, size=size, genre_id=genre, sort_=sort.value)
     if not films:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
 
     return [BaseFilm(**film.dict()) for film in films]
+
