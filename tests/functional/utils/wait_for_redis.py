609f1c5d6ad86949308dc5755343e5231ff66ded
PavelAd <adap2@mail.ru>
2023-01-26 19:28:32 +0400

    waiters для ES и Redis

--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,15 +1,13 @@
-from settings import settings
-
 import time
-
 from redis import Redis
+import sys; sys.path.append("..")
+from tests.functional.settings import settings
 
 if __name__ == '__main__':
     redis_client = Redis(
         host=settings.redis_host, port=settings.redis_port,
         decode_responses=True
     )
-
     while True:
         if redis_client.ping():
             break
