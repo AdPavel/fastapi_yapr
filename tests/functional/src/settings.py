6af3548c17f63523f75ec230b07ae88dbeb88eb8
PavelAd <adap2@mail.ru>
2023-01-26 10:24:46 +0400

    добавил настройки в одном месте

---
+++ tests/functional/src/settings.py
@@ -0,0 +1,23 @@
+import pathlib
+from pydantic import BaseSettings
+
+
+class Base(BaseSettings):
+    # Project section
+    project_name: str
+
+    # Elastic section
+    els_port: int
+    els_host: str
+
+    # Redis section
+    redis_host: str
+    redis_port: int
+
+    class Config:
+
+        env_file = f"{pathlib.Path(__file__).resolve().parent.parent.parent.parent}/.env"
+        env_file_encoding = 'utf-8'
+
+
+settings = Base()
