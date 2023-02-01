bf3eb24c48877489129e5d4037f2a8ce1d22af80
elena <ep.yudzina@gamil.com>
2023-02-01 11:56:01 +0500

    add params to test_genres_detail

--- tests/functional/src/test_genres.py
+++ tests/functional/src/test_genres.py
@@ -2,11 +2,24 @@ import pytest
 import uuid
 
 
+@pytest.mark.parametrize(
+    'query_data, expected_answer',
+    [
+        (
+                {'query': '/genres/640d1ac4-0f5a-465b-a75c-45941d28198b'},
+                {'status': 200, 'length': 3}
+        ),
+        (
+                {'query': '/genres/640d1ac4-0f5a-465b-a75c-45941d281900'},
+                {'status': 404, 'length': 1}
+        )
+    ]
+)
 @pytest.mark.asyncio
-async def test_genres_detail(make_request):
-    response = await make_request(endpoint='/genres/640d1ac4-0f5a-465b-a75c-45941d28198b')
-    assert response['status'] == 200
-    assert len(response['body']) == 3
+async def test_genres_detail(make_request, query_data, expected_answer):
+    response = await make_request(endpoint=query_data['query'])
+    assert response['status'] == expected_answer['status']
+    assert len(response['body']) == expected_answer['length']
 
 
 @pytest.mark.asyncio
