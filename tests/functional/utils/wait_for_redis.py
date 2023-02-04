a0781a11d255f66a5ee269e80040782c04c2fe4d
elena <ep.yudzina@gamil.com>
2023-02-04 17:31:43 +0500

    add backoff to redis and elastic connectors

--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,19 +1,26 @@
-import time
 from redis import Redis
 import sys
 import os
+import backoff
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 from settings import settings
 
 
-if __name__ == '__main__':
+@backoff.on_exception(
+    wait_gen=backoff.expo, exception=Exception
+)
+def wait_for_redis():
     redis_client = Redis(
         host=settings.redis_host, port=settings.redis_port,
         decode_responses=True
     )
-    while True:
-        if redis_client.ping():
-            break
-        time.sleep(1)
+    ping = redis_client.ping()
+    if ping:
+        return ping
+    raise Exception
+
+
+if __name__ == '__main__':
+    wait_for_redis()
