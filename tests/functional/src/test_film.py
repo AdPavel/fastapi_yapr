2a7784072fbb87930667ee0bfe0defe2b0f82ee1
PavelAd <adap2@mail.ru>
2023-02-04 13:00:22 +0400

    добавил HHTPStatus вместо численного обозначения, добавил тест на BAD_REQUEST для фильмов

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -1,16 +1,19 @@
 import pytest
 import json
+from http import HTTPStatus
+import uuid
+
 
 @pytest.mark.parametrize(
     'query_data, expected_answer',
     [
         (
                 {'query': '/films/647d1ac4-0f5a-465b-a75c-45941d28198b'},
-                {'status': 200, 'length': 8}
+                {'status': HTTPStatus.OK, 'length': 8}
         ),
         (
                 {'query': '/films/647d1ac4-0f5a-465b-a75c-45941d281999'},
-                {'status': 404, 'length': 1}
+                {'status': HTTPStatus.NOT_FOUND, 'length': 1}
         )
     ]
 )
@@ -24,7 +27,7 @@ async def test_get_film_by_id(make_request, query_data: dict, expected_answer: d
 @pytest.mark.asyncio
 async def test_get_films(make_request):
     response = await make_request(endpoint='/films/', params={'sort': 'imdb_rating', 'size': 50, 'page': 1})
-    assert response['status'] == 200
+    assert response['status'] == HTTPStatus.OK
     assert len(response['body']) == 50
 
 
@@ -37,9 +40,9 @@ async def test_get_sort_films(make_request):
     response_wrong_sorting_field = await make_request(endpoint='/films/',
                                                params={'sort': 'title', 'size': 50, 'page': 1})
 
-    assert response_wrong_sorting_field['status'] == 422
-    assert response_asc_sorting['status'] == 200
-    assert response_desc_sorting['status'] == 200
+    assert response_wrong_sorting_field['status'] == HTTPStatus.UNPROCESSABLE_ENTITY
+    assert response_asc_sorting['status'] == HTTPStatus.OK
+    assert response_desc_sorting['status'] == HTTPStatus.OK
     assert response_asc_sorting['body'][0]['imdb_rating'] != response_desc_sorting['body'][0]['imdb_rating']
 
 
@@ -48,11 +51,11 @@ async def test_get_sort_films(make_request):
     [
         (
                 {'query': 'The Star'},
-                {'status': 200, 'length': 50}
+                {'status': HTTPStatus.OK, 'length': 50}
         ),
         (
                 {'query': 'Mashed potato'},
-                {'status': 404, 'length': 1}
+                {'status': HTTPStatus.NOT_FOUND, 'length': 1}
         )
     ]
 )
@@ -68,7 +71,7 @@ async def test_cache_film(make_request, es_client, redis_client):
 
     film_id = '647d1ac4-0f5a-465b-a75c-45941d28198b'
     response_before_delete = await make_request(endpoint=f'/films/{film_id}')
-    assert response_before_delete['status'] == 200
+    assert response_before_delete['status'] == HTTPStatus.OK
 
     await es_client.delete('movies', film_id)
 
@@ -77,5 +80,17 @@ async def test_cache_film(make_request, es_client, redis_client):
 
     response_after_delete = await make_request(f'/films/{film_id}/')
     assert json.loads(redis_data) == response_before_delete['body']
-    assert response_after_delete['status'] == 200
+    assert response_after_delete['status'] == HTTPStatus.OK
     assert response_before_delete['body'] == response_after_delete['body']
+
+
+@pytest.mark.asyncio
+async def test_bad_request_film(es_client):
+
+    film_id = uuid.uuid4()
+    data = {'id': film_id, 'name': 'Test', 'role': 'wrong field'}
+
+    try:
+        await es_client.create('movies', film_id, data)
+    except:
+        assert HTTPStatus.BAD_REQUEST
