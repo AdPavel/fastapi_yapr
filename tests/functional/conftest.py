21016938ec2ccff784417f07e4f6634a232d7b58
PavelAd <adap2@mail.ru>
2023-02-04 12:58:48 +0400

    перенес создание данных в плагин

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,14 +1,12 @@
 import asyncio
 from redis import asyncio as rasyncio
-import json
 import aiohttp
 import pytest
 import pytest_asyncio
 from elasticsearch import AsyncElasticsearch
 from .settings import settings
-from .utils.es_helper import prepare_for_es_insert
 
-from .testdata.data import films_data, persons_data, genres_data
+pytest_plugins = 'plugins.createdata_fixtures'
 
 
 @pytest.fixture(scope="session")
@@ -26,8 +24,6 @@ async def es_client():
         host=settings.els_host,
         port=settings.els_port
     )
-    # !!! Обязательно заменить hosts, сейчас для теста
-    # client = AsyncElasticsearch(hosts=f'http://localhost:9200')
     client = AsyncElasticsearch(hosts=es_url)
     yield client
     await client.close()
@@ -49,36 +45,6 @@ async def session():
     session = aiohttp.ClientSession()
     yield session
     await session.close()
-
-
-async def create_data(es_client: AsyncElasticsearch, index: str, data: list[dict]):
-
-    els_schema_path = f'/functional/testdata/indexes/{index}.json'
-    # Для локального запуска
-    # els_schema_path = f'../testdata/indexes/{index}.json'
-    if not await es_client.indices.exists(index):
-        with open(els_schema_path, 'r') as file:
-            schema = json.load(file)
-        await es_client.indices.create(index=index, body=schema)
-    await es_client.indices.refresh(index=index)
-    response = await es_client.bulk(index=index, body=prepare_for_es_insert(data, index), refresh=True)
-    if response['errors']:
-        raise Exception('Ошибка записи данных в Elasticsearch')
-
-
-@pytest_asyncio.fixture(scope='session', autouse=True)
-async def create_films(es_client):
-    await create_data(es_client=es_client, index='movies', data=films_data.data)
-
-
-@pytest_asyncio.fixture(scope='session', autouse=True)
-async def create_persons(es_client):
-    await create_data(es_client=es_client, index='persons', data=persons_data.data)
-
-
-@pytest_asyncio.fixture(scope='session', autouse=True)
-async def create_genres(es_client):
-    await create_data(es_client=es_client, index='genres', data=genres_data.data)
 
 
 @pytest_asyncio.fixture
